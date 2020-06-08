4ed25d052231dff39d4818727948ca08dd4e9c2e
Rohit Saha <55949623+rohitmadrileno15@users.noreply.github.com>
2020-06-09 00:14:14 +0530

    Added Changes

--- auto-update.py
+++ auto-update.py
@@ -10,6 +10,7 @@ for line in read:
     array_of_packages.append(line)
 
 for _, name_of_package in enumerate(array_of_packages):
+    name_of_package = name_of_package.split("=",1)[0]
     call("pip install --upgrade " + name_of_package, shell=True)
     time.sleep(5)
     print('*****')
